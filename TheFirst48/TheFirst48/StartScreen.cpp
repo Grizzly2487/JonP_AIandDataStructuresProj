8acbe5874f598f58f6bd2baeb8cee1849e5d4f65
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2020-02-25 13:34:43 -0500

    Created The Project and Repo
    Have been sick all weekend and yesterday...i know im behind. will be working my butt off.

---
+++ TheFirst48/TheFirst48/StartScreen.cpp
@@ -0,0 +1,18 @@
+#include "StartScreen.h"
+
+using namespace std;
+
+StartScreen::StartScreen()
+{
+	cout << "\n\n";
+	cout << "\t\tThe First 48\n\n";
+	cout << "\tWhen a crime has been commited, the timer has begun.\n";
+	cout << "\tWhile most cases are solved within the first 48, some go on days, weeks, months, or even years after..\n";
+	cout << "\tAs a detective, you have only 48 hours, before the trail goes cold..\n";
+	cout << "\n";
+	cout << "Insert Story here\n";
+
+}
+
+StartScreen::~StartScreen()
+{}
