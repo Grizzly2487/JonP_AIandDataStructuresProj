261ca9907be06161e12508288f3819e8d2aacbb2
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2020-02-26 22:22:35 -0500

    Case1,2,3 override Functions
    Setup case1,2 and 3 and their override functions .

--- TheFirst48/TheFirst48/CaseOne.cpp
+++ TheFirst48/TheFirst48/CaseOne.cpp
@@ -1,1 +1,181 @@
 #include "CaseOne.h"
+
+void CaseOne::CaseDialogue()
+{
+}
+
+void CaseOne::SolvedDialogue()
+{
+}
+
+void CaseOne::NeutralDialogue()
+{
+}
+
+void CaseOne::AnxiousDialogue()
+{
+}
+
+void CaseOne::AngryDialogue()
+{
+}
+
+void CaseOne::SadDialogue()
+{
+}
+
+void CaseOne::ScaredDialogue()
+{
+}
+
+void CaseOne::Suspect1Dialogue()
+{
+}
+
+void CaseOne::Suspect2Dialogue()
+{
+}
+
+void CaseOne::Suspect3Dialogue()
+{
+}
+
+void CaseOne::Suspect4Dialogue()
+{
+}
+
+void CaseOne::Suspect5Dialogue()
+{
+}
+
+void CaseOne::Suspect6Dialogue()
+{
+}
+
+void CaseOne::Suspect1LiarDialogue()
+{
+}
+
+void CaseOne::Suspect2LiarDialogue()
+{
+}
+
+void CaseOne::Suspect3LiarDialogue()
+{
+}
+
+void CaseOne::Suspect4LiarDialogue()
+{
+}
+
+void CaseOne::Suspect5LiarDialogue()
+{
+}
+
+void CaseOne::Suspect6LiarDialogue()
+{
+}
+
+void CaseOne::Suspect1AccusedDialogue()
+{
+}
+
+void CaseOne::Suspect2AccusedDialogue()
+{
+}
+
+void CaseOne::Suspect3AccusedDialogue()
+{
+}
+
+void CaseOne::Suspect4AccusedDialogue()
+{
+}
+
+void CaseOne::Suspect5AccusedDialogue()
+{
+}
+
+void CaseOne::Suspect6AccusedDialogue()
+{
+}
+
+void CaseOne::Suspect1LADialogue()
+{
+}
+
+void CaseOne::Suspect2LADialogue()
+{
+}
+
+void CaseOne::Suspect3LADialogue()
+{
+}
+
+void CaseOne::Suspect4LADialogue()
+{
+}
+
+void CaseOne::Suspect5LADialogue()
+{
+}
+
+void CaseOne::Suspect6LADialogue()
+{
+}
+
+void CaseOne::Suspect1KillerDialogue()
+{
+}
+
+void CaseOne::Suspect2KillerDialogue()
+{
+}
+
+void CaseOne::Suspect3KillerDialogue()
+{
+}
+
+void CaseOne::Suspect4KillerDialogue()
+{
+}
+
+void CaseOne::Suspect5KillerDialogue()
+{
+}
+
+void CaseOne::Suspect6KillerDialogue()
+{
+}
+
+void CaseOne::Suspect1NKDialogue()
+{
+}
+
+void CaseOne::Suspect2NKDialogue()
+{
+}
+
+void CaseOne::Suspect3NKDialogue()
+{
+}
+
+void CaseOne::Suspect4NKDialogue()
+{
+}
+
+void CaseOne::Suspect5NKDialogue()
+{
+}
+
+void CaseOne::Suspect6NKDialogue()
+{
+}
+
+CaseOne::CaseOne()
+{
+}
+
+CaseOne::~CaseOne()
+{
+}
--- TheFirst48/TheFirst48/CaseThree.cpp
+++ TheFirst48/TheFirst48/CaseThree.cpp
@@ -1,1 +1,181 @@
 #include "CaseThree.h"
+
+void CaseThree::CaseDialogue()
+{
+}
+
+void CaseThree::SolvedDialogue()
+{
+}
+
+void CaseThree::NeutralDialogue()
+{
+}
+
+void CaseThree::AnxiousDialogue()
+{
+}
+
+void CaseThree::AngryDialogue()
+{
+}
+
+void CaseThree::SadDialogue()
+{
+}
+
+void CaseThree::ScaredDialogue()
+{
+}
+
+void CaseThree::Suspect1Dialogue()
+{
+}
+
+void CaseThree::Suspect2Dialogue()
+{
+}
+
+void CaseThree::Suspect3Dialogue()
+{
+}
+
+void CaseThree::Suspect4Dialogue()
+{
+}
+
+void CaseThree::Suspect5Dialogue()
+{
+}
+
+void CaseThree::Suspect6Dialogue()
+{
+}
+
+void CaseThree::Suspect1LiarDialogue()
+{
+}
+
+void CaseThree::Suspect2LiarDialogue()
+{
+}
+
+void CaseThree::Suspect3LiarDialogue()
+{
+}
+
+void CaseThree::Suspect4LiarDialogue()
+{
+}
+
+void CaseThree::Suspect5LiarDialogue()
+{
+}
+
+void CaseThree::Suspect6LiarDialogue()
+{
+}
+
+void CaseThree::Suspect1AccusedDialogue()
+{
+}
+
+void CaseThree::Suspect2AccusedDialogue()
+{
+}
+
+void CaseThree::Suspect3AccusedDialogue()
+{
+}
+
+void CaseThree::Suspect4AccusedDialogue()
+{
+}
+
+void CaseThree::Suspect5AccusedDialogue()
+{
+}
+
+void CaseThree::Suspect6AccusedDialogue()
+{
+}
+
+void CaseThree::Suspect1LADialogue()
+{
+}
+
+void CaseThree::Suspect2LADialogue()
+{
+}
+
+void CaseThree::Suspect3LADialogue()
+{
+}
+
+void CaseThree::Suspect4LADialogue()
+{
+}
+
+void CaseThree::Suspect5LADialogue()
+{
+}
+
+void CaseThree::Suspect6LADialogue()
+{
+}
+
+void CaseThree::Suspect1KillerDialogue()
+{
+}
+
+void CaseThree::Suspect2KillerDialogue()
+{
+}
+
+void CaseThree::Suspect3KillerDialogue()
+{
+}
+
+void CaseThree::Suspect4KillerDialogue()
+{
+}
+
+void CaseThree::Suspect5KillerDialogue()
+{
+}
+
+void CaseThree::Suspect6KillerDialogue()
+{
+}
+
+void CaseThree::Suspect1NKDialogue()
+{
+}
+
+void CaseThree::Suspect2NKDialogue()
+{
+}
+
+void CaseThree::Suspect3NKDialogue()
+{
+}
+
+void CaseThree::Suspect4NKDialogue()
+{
+}
+
+void CaseThree::Suspect5NKDialogue()
+{
+}
+
+void CaseThree::Suspect6NKDialogue()
+{
+}
+
+CaseThree::CaseThree()
+{
+}
+
+CaseThree::~CaseThree()
+{
+}
